66beac88a3d26bf1ead2cf7536dd52d8427964a8
Péter Szakszon <szp0530@gmail.com>
2021-12-25 17:43:27 +0100

    Simplify isDigit

--- go/next-int.go
+++ go/next-int.go
@@ -6,19 +6,11 @@
 // }
 
 func nextInt(b []byte, i int) (val, ni int) {
-    for ; i < len(b) && !isDigit(b[i]); i++ {
-    }
-    x := 0
-    for ; i < len(b) && isDigit(b[i]); i++ {
-        x = x*10 + int(b[i]) - '0'
-    }
-    return x, i
-}
-
-func isDigit(c byte) bool {
-    switch {
-    case '0' <= c && c <= '9':
-		return true
-    }
-    return false
+	for ; i < len(b) && !('0' <= b[i] && b[i] <= '9'); i++ {
+	}
+	x := 0
+	for ; i < len(b) && '0' <= b[i] && b[i] <= '9'; i++ {
+		x = x*10 + int(b[i]) - '0'
+	}
+	return x, i
 }
