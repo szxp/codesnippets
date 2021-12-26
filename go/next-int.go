297257a99d56ceb3361872174454367620623e33
Péter Szakszon <szp0530@gmail.com>
2021-12-26 18:25:44 +0100

    Parse negative integers

--- go/next-int.go
+++ go/next-int.go
@@ -6,11 +6,16 @@
 // }
 
 func nextInt(b []byte, i int) (val, ni int) {
-	for ; i < len(b) && !('0' <= b[i] && b[i] <= '9'); i++ {
+	for ; i < len(b) && !('0' <= b[i] && b[i] <= '9' || b[i] == '-'); i++ {
 	}
 	x := 0
-	for ; i < len(b) && '0' <= b[i] && b[i] <= '9'; i++ {
-		x = x*10 + int(b[i]) - '0'
+	sign := 1
+	for ; i < len(b) && ('0' <= b[i] && b[i] <= '9' || b[i] == '-'); i++ {
+		if b[i] == '-' {
+			sign = -1
+		} else {
+			x = x*10 + int(b[i]) - '0'
+		}
 	}
-	return x, i
+	return x * sign, i
 }
